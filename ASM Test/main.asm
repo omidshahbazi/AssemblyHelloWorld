ebff1453fd783e3b4904b8b8520189924b8d2243
Omid Shahbazi <sh.omid.m@gmail.com>
2021-06-20 13:51:00 +0430

    Modified   ASM Test/ASM Test.vcxproj Modified   ASM Test/main.asm

--- ASM Test/main.asm
+++ ASM Test/main.asm
@@ -5,7 +5,17 @@ ExitProcess proto, dwExitCode:dword
 
 .code
 main proc
-	mov eax, 5
+	mov eax, 2
+
+	mov esi, 0
+	mov ecx, 10
+	BEGIN_LOOP:
+	;
+	imul eax, 2
+	;
+	add esi, 1
+	cmp esi, ecx
+	jl BEGIN_LOOP
 
 	invoke ExitProcess, 0
 main endp
